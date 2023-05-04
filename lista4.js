1c3da11900455cb46d5a2280770f4558c935567e
LeoNoli <nollizleo@gmail.com>
2023-05-04 15:57:36 -0300

    Lista 2 dimensões

---
+++ lista4.js
@@ -0,0 +1,7 @@
+const alunos = ['Joao', 'Juliana', 'Caio','Ana'];
+const media = [10,8,7.5,9];
+
+const listaALMD = [alunos, media];
+
+console.log(`A aluna da posição 1 da Lista de alunos é ${listaALMD[0][1]}
+A nota dessa aluna é ${listaALMD[1][1]}`);
