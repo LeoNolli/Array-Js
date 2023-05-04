0e2f67561867e4d546202e6cb1ff2b42031dc860
LeoNoli <nollizleo@gmail.com>
2023-05-04 18:11:39 -0300

    CallBacks

---
+++ callbacks.js
@@ -0,0 +1,10 @@
+const nomes = ['Leo','Bruna','Alex'];
+
+//nomes.forEach((nome) =>{
+//    console.log(nome);
+//});
+
+function imprimeNome(nome){
+    console.log(nome);
+}
+nomes.forEach(imprimeNome);
