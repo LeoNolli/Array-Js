62f3ab3ad75c89c405673eae3a381dc98423a034
LeoNoli <nollizleo@gmail.com>
2023-05-04 16:44:01 -0300

    For and For-Of

---
+++ mediafor.js
@@ -0,0 +1,9 @@
+const notas = [10,6.5,8,7.5];
+let soma =0;
+
+for(i =0; i<notas.length; i++){
+    soma+=notas[i];
+}
+
+const media = soma /notas.length;
+console.log(`A média das notas é ${media}.`)
