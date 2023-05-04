a2a84c40db4cbf172d01affd8d37341d94ce0122
LeoNoli <nollizleo@gmail.com>
2023-05-04 15:50:39 -0300

    Concat

---
+++ lista3.js
@@ -0,0 +1,10 @@
+const salaJS = ['Evaldo','Camila','Mari'];
+const salaPython = ['Ju','Leo','Raquel'];
+
+
+//    uma nova const para retornar o valor          
+//                                concatena as const
+const salasjunstas= salaJS.concat(salaPython);
+
+console.log(salasjunstas);
+
