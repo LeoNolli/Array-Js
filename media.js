0bf4c851ca2b965d7d100b34b506a2a260649ea8
LeoNoli <nollizleo@gmail.com>
2023-05-04 15:10:24 -0300

    Arrays

---
+++ media.js
@@ -0,0 +1,15 @@
+//const n1 =10;
+//const n2 =6.5;
+//const n3 = 8
+//const n4=7.5;
+
+//posiçõe:0
+//              0  1   2  3 
+const notas = [10,6.5,8,7.5];
+
+const media= (notas[0]+notas[1]+notas[2]+notas[3]) / notas.length;
+
+//lenght dara o tamanho do array
+
+console.log(media);
+
