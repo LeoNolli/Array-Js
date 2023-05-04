36606a67785cad3a7ff5d4e7a2e864dd651d83df
LeoNoli <nollizleo@gmail.com>
2023-05-04 16:10:20 -0300

    Include and IndexOf

---
+++ aluno.js
@@ -0,0 +1,16 @@
+const alunos = ['Joao', 'Juliana', 'Caio','Ana'];
+const media = [10,8,7.5,9];
+
+const listaALMD = [alunos, media];
+
+function exibeNN(alunos){
+    if(listaALMD[0].includes(alunos)){
+        const indice = listaALMD[0].indexOf(alunos);
+        const mediaAL= listaALMD[1][indice];
+
+        console.log(`${alunos} tem a média ${mediaAL}.`);
+    }else{
+        console.log('Aluno não encontrado na base de dados!')
+    }
+}
+exibeNN('Juliana');
